d662244e0c0a8f0487150912c787ec2fc5a27780
Liam Mai <mvantruong.dev@gmail.com>
2025-02-19 22:01:46 +0700

    Update Login UI

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { interval, Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, NzInputModule, NzImageModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   providers: []
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   constructor() {}
 
-  listSlideImages = [
-    '/images/login-phones-item-1.png',
-    '/images/login-phones-item-2.png',
-    '/images/login-phones-item-3.png',
-    '/images/login-phones-item-4.png',
-  ];
+  showImageItem: number = 1;
+  private subscriptions: Subscription[] = []
+
+  ngOnInit(): void {
+    const imagesInterval = interval(5000);
+    const sub = imagesInterval.subscribe({
+      next: () => {
+        if (this.showImageItem === 4) {
+          this.showImageItem = 1;
+        } else {
+          this.showImageItem++;
+        }
+        console.log("🚀 ~ LoginComponent ~ ngOnInit ~ this.showImageItem:", this.showImageItem)
+      }
+    })
+
+    this.subscriptions.push(sub)
+  }
+
+
 }
